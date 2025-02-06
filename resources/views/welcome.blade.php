3f86e049b7fa6850ba71e36a59d88b30ef77fbd7
gaurav-sharma-3079 <gaurav.sharma@octobit8.com>
2025-02-06 19:15:28 +0530

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Gaurav-App</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        
+    </head>
+    <body>
+        <div>
+            <h1>Hello World!</h1>
+            <h2>This is a laravel project.</h2>
+        </div>
+    </body>
+</html>
